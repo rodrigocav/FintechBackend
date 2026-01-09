afd63785878cb9adc333f9ef2b9d6ce3489bbb1b
Rodrigo <rodrigo.cavicchioli@outlook.com>
2026-01-08 22:23:25 -0300

    Feat: Incluindo Services User e Category

--- src/main/java/com/API/backend/services/CategoryService.java
+++ src/main/java/com/API/backend/services/CategoryService.java
@@ -1,4 +1,25 @@
 package com.API.backend.services;
 
+
+
+
+import com.API.backend.dtos.category.CategoryResponse;
+import com.API.backend.entities.Category;
+import com.API.backend.repositories.CategoryRepository;
+
 public class CategoryService {
+    private final CategoryRepository repository;
+
+    public CategoryService(CategoryRepository repository){
+        this.repository = repository;
+    }
+    private CategoryResponse toResponse(Category category){
+        return new CategoryResponse(
+            category.getId(), 
+            category.getUser(),
+            category.getName(),
+            category.getIcon(),
+            category.getType()
+         );
+    }
 }
