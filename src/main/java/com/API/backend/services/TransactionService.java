218eddccdb4176087af726933a3aeee0d92a3426
GustavoMSandoval <gmsandoval2005@gmail.com>
2026-01-14 09:13:05 -0300

    ffix: Adicionando código do TransactionService

--- src/main/java/com/API/backend/services/TransactionService.java
+++ src/main/java/com/API/backend/services/TransactionService.java
@@ -1,5 +1,29 @@
 package com.API.backend.services;
 
+import org.springframework.stereotype.Service;
+
+import com.API.backend.dtos.transaction.TransactionResponse;
+import com.API.backend.entities.Transaction;
+import com.API.backend.repositories.TransactionRepository;
+
+@Service
 public class TransactionService {
+
+    private final TransactionRepository repository;
+
+    public TransactionService(TransactionRepository repository) {
+        this.repository = repository;
+    }
+
+    private TransactionResponse toResponse(Transaction transaction) {
+        return TransactionResponse(
+            transaction.getId(),
+            transaction.getDescription(),
+            transaction.getAmount(),
+            transaction.getType(),
+            transaction.getStatus(),
+            transaction.getUser()
+        );
+    }
     
 }
