fe6ff70a3dedace91afcee38b335a176f0585888
GustavoMSandoval <gmsandoval2005@gmail.com>
2026-01-14 12:57:46 -0300

    feat: Adicionando método de filtro de data, lista e criação em transaction

--- src/main/java/com/API/backend/services/TransactionService.java
+++ src/main/java/com/API/backend/services/TransactionService.java
@@ -1,7 +1,13 @@
 package com.API.backend.services;
 
+import java.sql.Date;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Service;
 
+import com.API.backend.dtos.transaction.TransactionRequest;
 import com.API.backend.dtos.transaction.TransactionResponse;
 import com.API.backend.entities.Transaction;
 import com.API.backend.repositories.TransactionRepository;
@@ -25,6 +31,32 @@ public class TransactionService {
             transaction.getStatus(),
             transaction.getDate()
         );
+    }
+
+    public TransactionResponse create(TransactionRequest request) {
+        Transaction transaction = new Transaction(
+            request.user(),
+            request.description(),
+            request.amount(),
+            request.type(),
+            request.status()
+        );
+        return toResponse(repository.save(transaction));
+    }
+
+    public List<TransactionResponse> list() {
+        return repository.findAll()
+                .stream()
+                .map(this::toResponse)
+                .toList();
+    }
+
+    @Query(value  = "SELECT * FROM transactions t WHERE t.date BETWEEN :startDate AND :endDate", nativeQuery = true)
+    public List<TransactionResponse> filterByDate(@Param("startDate") Date startDate, @Param("endDate") Date endDate) {
+        return repository.findAll()
+               .stream()
+               .map(this::toResponse)
+               .toList();
     }
     
 }
